b5ecd30577dfd1037d778efca6e97e286a3206c7
ramiskaramatov <ramis7861@hotmail.com>
2025-06-25 18:37:14 +0100

    Removed welcome message

--- src/js/controller.js
+++ src/js/controller.js
@@ -127,10 +127,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log("Welcome to the application");
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -139,7 +135,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 
 init();
